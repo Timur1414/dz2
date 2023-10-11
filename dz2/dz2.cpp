dafc660a7c8d32e89b97720f0165d144a578f3c5
Timur1414 <63254053+Timur1414@users.noreply.github.com>
2023-10-11 21:20:37 +0300

    init

---
+++ dz2/dz2.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+#include <string>
+
+
+using namespace std;
+
+
+class People {
+private:
+	string gender;
+	int age;
+	int death_probability;
+	int offspring_probability;
+	int food_consumption;
+public:
+
+};
+
+
+int main() {
+	int n, m;
+	cout << "Enter food reserve (n):\n";
+	cin >> n;
+	cout << "Enter food purchase (m):\n";
+	cin >> m;
+
+
+	return 0;
+}
